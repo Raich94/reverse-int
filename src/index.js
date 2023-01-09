6da4770e7cd57d329eab0ff1b927bb2cfa34d2df
Raich94 <raichxxx@mail.ru>
2023-01-09 23:23:19 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let plus = Math.abs(n);
+    for (let i = 0; i < plus.toString().length; i++){
+        result = `${plus.toString()[i]}${result}`
+    }
+  return Number(result);
 }
